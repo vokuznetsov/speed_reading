576fd711bf915430df2da54ef98722f1cd9a9941
Vladimir Kuznetsov <vokuznecov@gmail.com>
2019-11-03 00:48:54 +0300

    print text logic

--- js/app.js
+++ js/app.js
@@ -12,9 +12,41 @@ function addText(event) {
     const textInput = document.getElementById('text-input');
     const text = textInput.value;
 
-    if (text === '') {
+    const speedInput = document.getElementById('speed-input');
+    const speed = speedInput.value;
+
+    if (text === '' || speed === undefined) {
         return alert("Необходимо ввести название задачи");
     }
 
+    const textField = document.querySelector('.textfield');
+    const textArr = text.split(/[\s,]+/);
+
+    if (textArr.length > 3) {
+        printWords(0, textArr, textField, speed);
+    } else {
+        textField.innerHTML = textInput;
+    }
+
+
+
     console.log(text);
 }
+
+function printWords(i, textArr, textField, speed) {
+    setTimeout(function () {
+        if (i < textArr.length) {
+            var outputWords = [];
+            for (; outputWords.length < 3 && i < textArr.length; i++) {
+                outputWords.push(textArr[i]);
+            }
+
+            var text = outputWords.join(' ');
+            textField.innerHTML = text;
+            //console.log(text);
+
+            printWords(i, textArr, textField, speed);
+        }
+    }, speed)
+}
+
